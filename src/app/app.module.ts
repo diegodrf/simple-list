3050d6e4db279386a61d2eb3ae8d77878e67848b
Diego Faria <diego.rdfaria@gmail.com>
2023-03-19 04:50:25 -0400

    created navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsListComponent } from './components/items-list/items-list.component';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsListComponent
+    ItemsListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
