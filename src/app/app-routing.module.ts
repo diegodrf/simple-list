79f8eac954aca13e14fa99304b9cae6a5d5d3e0c
Diego Faria <diego.rdfaria@gmail.com>
2023-03-19 05:49:11 -0400

    added router to new item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemsListComponent } from './pages/items-list/items-list.component';
 import { ItemDetailComponent } from './pages/item-detail/item-detail.component';
+import { NewItemComponent } from './pages/new-item/new-item.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,10 @@ const routes: Routes = [
       {
         'path': ':id',
         'component': ItemDetailComponent
+      },
+      {
+        'path': 'new',
+        'component': NewItemComponent
       }
     ]
   }
