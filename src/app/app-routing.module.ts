6ee5f91fb26229ed6689aeebc6823065396edd68
Diego Faria <diego.rdfaria@gmail.com>
2023-03-19 05:11:47 -0400

    create route for detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemsListComponent } from './pages/items-list/items-list.component';
+import { ItemDetailComponent } from './pages/item-detail/item-detail.component';
 
 const routes: Routes = [
   {
     'path': '',
     'component': ItemsListComponent
+  },
+  {
+    'path': 'items',
+    'children': [
+      {
+        'path': ':id',
+        'component': ItemDetailComponent
+      }
+    ]
   }
 ];
 
