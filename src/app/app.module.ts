1b01a3cf2553abb3e70885ce260e84c2fbcdf110
Diego Faria <diego.rdfaria@gmail.com>
2023-03-19 05:11:29 -0400

    created item detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsListComponent } from './pages/items-list/items-list.component';
 import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { ItemDetailComponent } from './pages/item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsListComponent,
-    NavBarComponent
+    NavBarComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule,
