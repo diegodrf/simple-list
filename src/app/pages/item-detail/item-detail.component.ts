75d39d55390eb5359a4187502340a7329c59dab0
Diego Faria <diego.rdfaria@gmail.com>
2023-03-19 05:25:18 -0400

    showing item in item detail route

--- src/app/pages/item-detail/item-detail.component.ts
+++ src/app/pages/item-detail/item-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Item } from 'src/app/models/item';
+import { LocalStorageRepositoryService } from 'src/app/services/local-storage-repository.service';
 
 @Component({
   selector: 'app-item-detail',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemDetailComponent implements OnInit {
 
-  constructor() { }
+  item!: Item;
+  constructor(
+    private _localStorageRepository: LocalStorageRepositoryService,
+    private _activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this._activatedRoute.snapshot.params['id']
+    this.item = this._localStorageRepository.getItemById(id);
   }
 
 }
